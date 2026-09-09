e4191c075cd25ee1442a0abb40dbb80ea4831341
mustapha-rashiduddin <chumchumchira@gmail.com>
2026-09-09 11:02:48 +0500

    st: patch Gruvbox-inspired high-contrast color palette

--- users/saifr/home.nix
+++ users/saifr/home.nix
@@ -126,6 +126,29 @@ xclip # (Optional: for tiny copy/paste)
 	# Tell st not to artificially "smear" the regular font by overstriking it
         #sed -i 's/badweight = 1/badweight = 0/g' x.c
 
+        # High-contrast color scheme (Gruvbox-inspired, brighter than defaults)
+        # Foreground / background
+        sed -i 's/static const unsigned int defaultfg = .*/static const unsigned int defaultfg = 7;/' config.def.h
+        sed -i 's/static const unsigned int defaultbg = .*/static const unsigned int defaultbg = 0;/' config.def.h
+        sed -i 's/static const unsigned int defaultcs = .*/static const unsigned int defaultcs = 258;/' config.def.h
+        # Palette (indices 0-15) – only match the hex color, preserve trailing comment
+        sed -i 's/\[0\] = "#[0-9a-fA-F]*"/[0] = "#282828"/' config.def.h
+        sed -i 's/\[1\] = "#[0-9a-fA-F]*"/[1] = "#cc241d"/' config.def.h
+        sed -i 's/\[2\] = "#[0-9a-fA-F]*"/[2] = "#98971a"/' config.def.h
+        sed -i 's/\[3\] = "#[0-9a-fA-F]*"/[3] = "#d79921"/' config.def.h
+        sed -i 's/\[4\] = "#[0-9a-fA-F]*"/[4] = "#458588"/' config.def.h
+        sed -i 's/\[5\] = "#[0-9a-fA-F]*"/[5] = "#b16286"/' config.def.h
+        sed -i 's/\[6\] = "#[0-9a-fA-F]*"/[6] = "#689d6a"/' config.def.h
+        sed -i 's/\[7\] = "#[0-9a-fA-F]*"/[7] = "#a89984"/' config.def.h
+        sed -i 's/\[8\] = "#[0-9a-fA-F]*"/[8] = "#928374"/' config.def.h
+        sed -i 's/\[9\] = "#[0-9a-fA-F]*"/[9] = "#fb4934"/' config.def.h
+        sed -i 's/\[10\] = "#[0-9a-fA-F]*"/[10] = "#b8bb26"/' config.def.h
+        sed -i 's/\[11\] = "#[0-9a-fA-F]*"/[11] = "#fabd2f"/' config.def.h
+        sed -i 's/\[12\] = "#[0-9a-fA-F]*"/[12] = "#83a598"/' config.def.h
+        sed -i 's/\[13\] = "#[0-9a-fA-F]*"/[13] = "#d3869b"/' config.def.h
+        sed -i 's/\[14\] = "#[0-9a-fA-F]*"/[14] = "#8ec07c"/' config.def.h
+        sed -i 's/\[15\] = "#[0-9a-fA-F]*"/[15] = "#ebdbb2"/' config.def.h
+
       '';
     }))
     #dash
